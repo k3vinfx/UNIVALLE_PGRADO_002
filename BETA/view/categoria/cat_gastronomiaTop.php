87566ecca96e6a20a6e986b3fa2ffd52828df53a
Kevin Carrillo <k3vinfx@gmail.com>
2024-07-11 22:06:18 -0400

    fix 11/07/2024 pestañas en buscada de lugares.

--- BETA/view/categoria/cat_gastronomiaTop.php
+++ BETA/view/categoria/cat_gastronomiaTop.php
@@ -41,6 +41,20 @@
                 <i class="fa fa-search" style="font-size: 20px;"></i> ¿Dónde quieres ir?
               </h5>
               <div class="input-group mb-4">
+
+              <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                  <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Inicio</button>
+                  <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Perfil</button>
+                  <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contacto</button>
+                </div>
+              </nav>
+              <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+              </div>
+
               <select name="precio" id="precio"  class="form-select-outline-secondary">
                     <option value="1">Muy Económico</option>
                     <option value="2">Económico</option>
