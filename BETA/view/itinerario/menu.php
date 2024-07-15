550e40a3b87162522ee10da197a521bc06e7455f
Kevin Carrillo <k3vinfx@gmail.com>
2024-07-14 23:05:13 -0400

    fix 14/07/2024 pestañas en buscada de lugares.

--- BETA/view/itinerario/menu.php
+++ BETA/view/itinerario/menu.php
@@ -1,5 +1,5 @@
 <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
-<link rel="stylesheet" href="../assets/css/all.min.css">
+<link rel="stylesheet" href="../BETA/assets/css/all.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
