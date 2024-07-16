12e0c1c516e552146b354f47d1c01cc1c600a11e
Kevin Carrillo <k3vinfx@gmail.com>
2024-07-16 19:57:29 -0400

    fix 14/07/2024 pestañas en buscada de lugares.

--- BETA/model/categoria.php
+++ BETA/model/categoria.php
@@ -300,6 +300,38 @@ class categoria
 			die($e->getMessage());
 		}
 	}
+	public function MenuLista_2()
+	{
+		try
+		{
+
+			$result = array();
+			$stm = $this->pdo->prepare("SELECT
+			a.Recomendacion_id AS ID,
+			a.Recomendacion_titulo AS TITULO,
+			b.Categoria_nombre AS categorias,
+			a.Recomendacion_costo AS COSTO,
+			a.Recomendacion_estado AS ESTADO,
+			a.Recomendacion_descripcion AS DESCRIPCION,
+			c.Recomendacion_Img1 AS CARGA1,
+			a.Recomendacion_fecha_creacion AS FECHA
+		FROM
+			recomendacion a
+			INNER JOIN categoria b ON a.Recomendacion_categoria = b.Categoria_id
+			INNER JOIN recomendacion_img c ON a.Recomendacion_id = c.Recomendacion_FK
+		WHERE a.Recomendacion_categoria = 2
+		ORDER BY
+			a.Recomendacion_id DESC;
+			");
+			$stm->execute();
+			return $stm->fetchAll(PDO::FETCH_OBJ);
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+
 
 
 	public function MenuListaBusqueda()
