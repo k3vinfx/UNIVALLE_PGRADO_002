63f6c381ef9c19e2b621489239f016e958018865
midiario <anhimargot@gmail.com>
2024-08-04 17:15:22 -0400

    error simple 1

--- view/alternativa/alternativa.php
+++ view/alternativa/alternativa.php
@@ -68,7 +68,7 @@ input:checked + .slider:before {
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-6">
 		<h1 class="h3 mb-0 text-gray-800">Alternativa</h1>
-		<a href="registro_cliente.php" class="btn btn-primary">Nuevo</a>
+		<a href="?c=alternativa&a=Nuevo" class="btn btn-primary">Nuevo</a>
 	</div>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
