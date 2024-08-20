baf55c1e2e6d38e00d0feb80eeec385dc0e7bfaf
Kevin Carrillo <k3vinfx@gmail.com>
2024-08-19 23:27:01 -0400

    RRN PARTE 1

--- BETA/controller/inicio.controller.php
+++ BETA/controller/inicio.controller.php
@@ -318,7 +318,7 @@ class InicioController{
                 
                         ?> 
              
-                    
+             <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
                         <script>
                        
                         Swal.fire({
