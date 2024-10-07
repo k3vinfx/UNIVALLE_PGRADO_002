b61bd5484e7002e55327189159726310653e13e4
Kevin Carrillo <k3vinfx@gmail.com>
2024-10-06 22:12:33 -0400

    ERROR BUG 3 reparando prueba en alternativas turisticas para dejar en los demas vistas x10

--- controller/categorias.controller.php
+++ controller/categorias.controller.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once 'model/categorias.php';
 
 class CategoriasController{
@@ -9,9 +10,32 @@ class CategoriasController{
         $this->model = new categorias();
     }
 
+     // Método privado para verificar la sesión
+     private function verificarSesion(){
+        // Verificar si el usuario está autenticado
+        if (!isset($_SESSION['session_email']) || empty($_SESSION['session_email'])) {
+            // Si no está autenticado, redirigir al inicio de sesión
+            header("Location: index.php");
+            exit();
+        }
+    }
+
+    // Método privado para evitar el almacenamiento en caché
+    private function evitarCache(){
+        // Evitar que el navegador almacene en caché la página
+        header("Expires: Tue, 01 Jan 2000 00:00:00 GMT"); // Fecha en el pasado
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // Siempre modificado
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
+    }
+
     //Llamado plantilla alternativa
     public function Listar(){
-       require_once 'view/alternativa/header.php';
+        $this->verificarSesion(); // Verificar si el usuario está autenticado
+        $this->evitarCache();     
+        
+       require_once 'view/pago/header.php';
        require_once 'view/categorias/listar.php';
        require_once 'view/footerx.php';
     }
@@ -19,7 +43,7 @@ class CategoriasController{
     public function abrirNuevo(){
         // $pvd = new categorias();
 
-        require_once 'view/alternativa/header.php';
+        require_once 'view/pago/header.php';
         require_once 'view/categorias/nuevo.php';
         require_once 'view/footerx.php';
     }
@@ -45,7 +69,7 @@ class CategoriasController{
                 ],
             ];
         }
-        require_once 'view/alternativa/header.php';      
+        require_once 'view/pago/header.php';      
         require_once 'view/categorias/editar.php';
         require_once 'view/footerx.php';
     }
@@ -54,7 +78,7 @@ class CategoriasController{
     public function Entrenar(){
         $pvd = new categorias();
 
-        require_once 'view/alternativa/header.php';
+        require_once 'view/pago/header.php';
         require_once 'view/alternativa/entrenar.php';
         require_once 'view/footerx.php';
     }
@@ -65,7 +89,7 @@ class CategoriasController{
         if(isset($_REQUEST['Recomendacion_id'])){
             $pvd = $this->model->ObtenerX($_REQUEST['Recomendacion_id']);
         }
-        require_once 'view/alternativa/header.php';      
+        require_once 'view/pago/header.php';      
         require_once 'view/alternativa/editar.php';
         require_once 'view/footerx.php';
     }
@@ -75,7 +99,7 @@ class CategoriasController{
         if(isset($_REQUEST['Neurona_Id'])){
             $pvd = $this->model->Obtener($_REQUEST['Neurona_Id']);
         }
-        require_once 'view/alternativa/header.php';      
+        require_once 'view/pago/header.php';      
         require_once 'view/alternativa/alternativa-editar-entrenamiento.php';
         require_once 'view/footerx.php';
     }
@@ -85,7 +109,7 @@ class CategoriasController{
         $alert = '<div class="alert alert-primary" role="alert">
                          Registrado
                     </div>';
-        require_once 'view/alternativa/header.php';
+        require_once 'view/pago/header.php';
         require_once 'view/alternativa/nuevo.php';
         require_once 'view/footerx.php';
     }
