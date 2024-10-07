87e9640a328da1e3506eafbef6b8b56be2fa4935
Kevin Carrillo <k3vinfx@gmail.com>
2024-10-06 21:55:15 -0400

    ERROR BUG 3 reparando prueba en alternativas turisticas para dejar en los demas vistas x5

--- controller/alternativa.controller.php
+++ controller/alternativa.controller.php
@@ -34,11 +34,11 @@ class AlternativaController{
 
     //Llamado plantilla alternativa
     public function Index(){
-       // $pvd = new alternativa();
-       //$this->verificarSesion(); // Verificar si el usuario está autenticado
-       //$this->evitarCache();     // Evitar el almacenamiento en caché
+        $pvd = new alternativa();
+       $this->verificarSesion(); // Verificar si el usuario está autenticado
+       $this->evitarCache();     // Evitar el almacenamiento en caché
 
-       require_once 'view/alternativa/header.php';
+       require_once 'view/pago/header.php';
        require_once 'view/alternativa/alternativa.php';
        require_once 'view/footerx.php';
     } 
