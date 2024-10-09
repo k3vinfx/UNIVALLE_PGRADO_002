80fe0db6433ac2cbdb34d2e8fca3522aedc77052
Kevin Carrillo <k3vinfx@gmail.com>
2024-10-09 00:07:23 -0400

    ERROR BUG 4 centrar todo en el medio x3

--- BETA/view/inicio/inicio.php
+++ BETA/view/inicio/inicio.php
@@ -80,7 +80,7 @@ body {
   <div class="container" style="margin-top: 100px">
     <div class="row" style="margin-bottom: 20px; display: flex; justify-content: center; text-align: center;">>
       <div class="row" style="margin-bottom: 20px">
-        <h2 class="underline white-text bold center">Información</h2>
+        <h2 class="underline white-text bold center">Información..</h2>
     
 
 
