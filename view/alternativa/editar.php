f2f65a7e29958e3894b7a1dda2ea3d6c18cc35b0
k3vinfx <k3vinfx@gmail.com>
2024-11-19 23:34:45 -0400

    prueba 2

--- view/alternativa/editar.php
+++ view/alternativa/editar.php
@@ -421,7 +421,7 @@ table{
                         <option value="0" <?php echo isset($status) && $status == 0 ? 'selected' : '' ?>>Inactivo</option>
                         </select>
                         </div>
-                        <div class="form-group col-md-5">
+                        <div class="form-group col-md-4">
                         <input type="submit" value="Guardar" class="btn btn-primary">
                         <a href="?c=alternativa" class="btn btn-danger">Regresar</a>
 
